a2fa272dd08950dfac748719c9a65f0f70014a2e
Daniel Galinkin <daniel.galinkin@dhexa.com>
2010-10-28 10:35:47 -0200

    Começando as classes de FamiliarStranger
    Criei uma interface, FamiliarStranger.m, que define a função para
    calcular a matriz de estranhos familiares.
    Criei também o arquivo FamiliarStrangerSimple.m, que conterá a primeira
    implementação mais simples para calcular esta matriz.

---
+++ src/FamiliarStrangerSimple.m
@@ -0,0 +1,15 @@
+classdef FamiliarStrangerSimple < FamiliarStranger
+    %FamiliarStrangerSimple Provides a simple implementation for the
+    %FamiliarStranger interface
+    
+    properties
+    end
+    
+    methods
+        function familiarStrangerGraph = calculateFamiliarStrangerGraph(s, network)
+            familiarStrangerGraph = [];
+        end
+    end
+    
+end
+

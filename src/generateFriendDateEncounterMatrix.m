e1a8e0ad557661aa88079c8e0500ce5273fc2ee2
Daniel Galinkin <daniel.galinkin@dhexa.com>
2010-11-18 10:45:58 -0200

    aa

---
+++ src/generateFriendDateEncounterMatrix.m
@@ -0,0 +1,28 @@
+function result = generateFriendDateEncounterMatrix(s,network,encounterMatrix)
+    import java.lang.*;
+    import java.util.*;
+    
+    result = HashMap();
+    for i=1:92
+        n1 = network.sub_sort(i);
+        mac1 = int64(hex2dec(s(n1).my_mac));
+        disp(mac1);
+        result.put(mac1, HashMap());
+        actualHash = encounterMatrix.get(mac1);
+        if(isempty(actualHash))
+            continue;
+        end
+        auxHash = result.get(mac1);
+        
+        for j=1:92
+            n2 = network.sub_sort(j);
+            mac2 = int64(hex2dec(s(n2).my_mac));
+            if(network.friends(i,j) == 1)
+                tmp = actualHash.get(mac2);
+                if(~isempty(tmp))
+                    auxHash.put(mac2, tmp);
+                end
+            end
+        end
+    end
+end
--- src/teste.m
+++ src/teste.m
@@ -1,6 +1,31 @@
 workMatrix = generateEncounterMatrix( s, network, 'home');
 homeMatrix = generateEncounterMatrix( s, network, 'home');
 elsewhereMatrix = generateEncounterMatrix( s, network, 'elsewhere');
+
 workPairs = encounterPairs(workMatrix, 0, 9999999);
 homePairs = encounterPairs(homeMatrix, 0, 9999999);
 elsewherePairs = encounterPairs(elsewhereMatrix, 0, 9999999);
+
+workDateMatrix = generateDateEncounterMatrix(s,network,'work');
+homeDateMatrix = generateDateEncounterMatrix(s,network,'home');
+elsewhereDateMatrix = generateDateEncounterMatrix(s,network,'elsewhere');
+
+workWeeks = weeks(s,network,workDateMatrix,0,9999);
+homeWeeks = weeks(s,network,homeDateMatrix,0,9999);
+elsewhereWeeks = weeks(s,network,elsewhereDateMatrix,0,9999);
+
+workFriendDateMatrix = generateFriendDateEncounterMatrix(s,network, workDateMatrix);
+homeFriendDateMatrix = generateFriendDateEncounterMatrix(s,network,homeDateMatrix);
+elsewhereFriendDateMatrix = generateFriendDateEncounterMatrix(s,network,elsewhereDateMatrix);
+
+workNonFriendDateMatrix = generateNonFriendDateEncounterMatrix(s,network,workDateMatrix);
+homeNonFriendDateMatrix = generateNonFriendDateEncounterMatrix(s,network,homeDateMatrix);
+elsewhereNonFriendDateMatrix = generateNonFriendDateEncounterMatrix(s,network,elsewhereDateMatrix);
+
+workFriendWeeks = weeks(s,network,workFriendDateMatrix,0,9999);
+homeFriendWeeks = weeks(s,network,homeFriendDateMatrix,0,9999);
+elsewhereFriendWeeks = weeks(s,network,elsewhereFriendDateMatrix,0,9999);
+
+workNonFriendWeeks = weeks(s,network,workNonFriendDateMatrix,0,9999);
+homeNonFriendWeeks = weeks(s,network,homeNonFriendDateMatrix,0,9999);
+elsewhereNonFriendWeeks = weeks(s,network,elsewhereNonFriendDateMatrix,0,9999);
